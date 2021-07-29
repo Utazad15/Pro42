fbbffe33f01c042a6eea1655fdd265576e2e08bd
Utazad15 <80836314+Utazad15@users.noreply.github.com>
2021-07-29 14:00:40 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,14 +11,18 @@ let ship2Img;
 let ship3Img;
 let ship4Img; 
 let hasDocked = false;
+let box;
+let PLAY = 1;
+let END = 0;
+let gameState = PLAY;
 
 function preload(){
   bg = loadImage("images/spacebg.jpg");
   mainShipImg = loadImage("images/iss.png");
-  ship1Img = loadAnimation("images/spacecraft1.png");
-  ship2Img = loadAnimation("images/spacecraft2.png");
-  ship3Img = loadAnimation("images/spacecraft3.png");
-  ship4Img = loadAnimation("images/spacecraft4.png");
+  ship1Img = loadImage("images/spacecraft1.png");
+  ship2Img = loadImage("images/spacecraft2.png");
+  ship3Img = loadImage("images/spacecraft3.png");
+  ship4Img = loadImage("images/spacecraft4.png");
 }
 
 function setup() {
@@ -26,40 +30,57 @@ function setup() {
   mainShip = createSprite(400, 280, 50, 50);
   mainShip.addImage("mainship",mainShipImg);
   ship = createSprite(330,425,50,50);
-  ship.addAnimation("ship1",ship1Img);
-  ship.addAnimation("ship2",ship2Img);
-  ship.addAnimation("ship3",ship3Img);
-  ship.addAnimation("ship4",ship4Img);
+  ship.addImage("ship1",ship1Img);
+  ship.addImage("ship2",ship2Img);
+  ship.addImage("ship3",ship3Img);
+  ship.addImage("ship4",ship4Img);
   ship.scale = 0.25;
+  //ship.debug = true;
+  ship.setCollider("rectangle",-10,-300,50,50);
+
+  box = createSprite(333,315,10,10);
+  box.visible = false;
 }
 
 function draw() {
-  background(bg);  
+  background(bg);
+  
+  drawSprites(); 
 
-  if(hasDocked){
-    ship.x = Math.round(random(320,340));
+  if(gameState === PLAY){
 
+    if(hasDocked){
+      ship.x = random(320,340);
+      ship.y = random(390,410);
+      
+    }
+  
     if(keyDown("left")){
       ship.x = ship.x - 1;
-      ship.changeAnimation("ship3",ship3Img);
+      ship.changeImage("ship3",ship3Img);
     }
   
     if(keyDown("right")){
       ship.x = ship.x + 1;
-      ship.changeAnimation("ship4",ship4Img);
+      ship.changeImage("ship4",ship4Img);
     }
   
     if(keyDown("up")){
       ship.y = ship.y - 1;
-      ship.changeAnimation("ship1",ship1Img);
+      ship.changeImage("ship1",ship1Img);
     }
   
     if(keyDown("down")){
-     // ship.x = ship.y + 1;
-      ship.changeAnimation("ship2",ship2Img);
+     // ship.y = ship.y + 1;
+      ship.changeImage("ship2",ship2Img);
     }
-    
-  }
 
-  drawSprites();
+    if(ship.isTouching(box)){
+     gameState = END;
+    }
+
+  } else if(gameState === END){
+    textSize(50);
+    text("Docking Successful!",50,520);
+  }
 }
